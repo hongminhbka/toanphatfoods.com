298496676f7509a2578d3a5a5edbdce0d4e8b5e3
hongminhbka <hongminhbka@gmail.com>
2022-08-27 17:01:41 +0700

    edit search

--- wp-content/themes/finance/tpl/header/header-style1.php
+++ wp-content/themes/finance/tpl/header/header-style1.php
@@ -10,7 +10,9 @@
 
                     <?php if ( themesflat_choose_opt('header_searchbox') == 1 ) :?>
                     <div class="show-search">
-                        <a href="#"><i class="fa fa-search"></i></a>         
+                        <div class="submenu top-search widget_search">
+                            <?php get_search_form(); ?>
+                        </div>         
                     </div> 
                     <?php endif;?>
 
@@ -18,10 +20,7 @@
                         get_template_part( 'tpl/header/navigator');
                     ?>
                               
-                </div><!-- /.header-wrap -->
-                <div class="submenu top-search widget_search">
-                    <?php get_search_form(); ?>
-                </div> 
+                </div>                 
             </div><!-- /.col-md-12 -->
         </div><!-- /.row -->
     </div><!-- /.container -->    
